83edeb74675ec24bbf4871c870dfbb7b57229035
Robert Jaskólski <rjaskolski@i2asolutions.com>
2021-12-13 15:16:14 +0100

    Add video

--- src/app/modules/video-capture/video-capture.component.ts
+++ src/app/modules/video-capture/video-capture.component.ts
@@ -1,13 +1,33 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 @Component({
   selector: 'app-video-capture',
   templateUrl: './video-capture.component.html',
   styleUrls: ['./video-capture.component.scss']
 })
 export class VideoCaptureComponent implements OnInit {
+  @ViewChild('video') videoElementRef: any;
+  videoElement: any;
+  // mediaRecorder: any;
+  // recordedBlobs: Blob[];
+  // isRecording: boolean = false;
+  // downloadUrl: string;
+  stream: any;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+  }
+
+  async ngOnInit() {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: false,
+      })
+      .then(stream => {
+        this.videoElement = this.videoElementRef.nativeElement;
+
+        this.stream = stream;
+        this.videoElement.srcObject = this.stream;
+      });
   }
 }
