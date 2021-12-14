7f5843bedc6ec85affb5a81a09b99ac35c6ccb57
Robert Jaskólski <rjaskolski@i2asolutions.com>
2021-12-14 13:13:14 +0100

    update video method

--- src/app/modules/video-capture/video-capture.component.ts
+++ src/app/modules/video-capture/video-capture.component.ts
@@ -11,81 +11,95 @@ export class VideoCaptureComponent implements OnInit {
   videoElement: any;
   stream: any;
   isRecording: boolean = false;
-  recordedBlobs: any[] = [];
-  mediaRecorder: any;
-  downloadUrl: string = '';
-  recordVideoElement: any;
+  devicesInfo: any;
 
   constructor() {
   }
 
   async ngOnInit() {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-        audio: true,
-      })
-      .then(stream => {
-        this.videoElement = this.videoElementRef.nativeElement;
-        this.videoElement.volume = 0;
-        this.recordVideoElement = this.recordVideoElementRef.nativeElement;
-        this.stream = stream;
-        this.videoElement.srcObject = this.stream;
+    const deives = await this.getDevices();
+    this.gotDevices(deives);
+  }
+
+  getDevices() {
+    console.log(navigator.mediaDevices.enumerateDevices());
+    return navigator.mediaDevices.enumerateDevices();
+  }
+
+  gotDevices(devicesInfo: any) {
+    this.devicesInfo = devicesInfo;
+    const audioSource = devicesInfo.find((device: any) => device.kind === 'audioinput');
+    const videoSource = devicesInfo.find((device: any) => device.kind === 'videoinput');
+    if (this.stream) {
+      this.stream.getTracks().forEach((track: any) => {
+        track.stop();
       });
-  }
-
-  record() {
-    if (this.isRecording) {
-      this.stopRecording()
-    } else {
-      this.startRecording()
     }
-  }
-
-  startRecording() {
-    this.recordedBlobs = [];
-    let options: any = { mimeType: 'video/webm'};
-
-    try {
-      this.mediaRecorder = new MediaRecorder(this.stream, options);
-    } catch (err) {
-      console.log(err);
+    const constrants = {
+      audio: {deviceId: audioSource.deviceId ? {exact: audioSource.deviceId} : undefined},
+      video: {deviceId: videoSource.deviceId ? {exact: videoSource.deviceId} : undefined},
     }
-
-    this.mediaRecorder.start(); // collect 100ms of data
-    this.isRecording = !this.isRecording;
-    this.onDataAvailableEvent();
-    this.onStopRecordingEvent();
+    return navigator.mediaDevices.getUserMedia(constrants).then(result => this.gotStream(result)).catch((err) => console.log(err));
   }
 
-  stopRecording() {
-    this.mediaRecorder.stop();
-    this.isRecording = !this.isRecording;
+  gotStream(stream: any) {
+    this.videoElement = this.videoElementRef.nativeElement;
+    this.stream = stream;
+    this.videoElement.srcObject = stream;
   }
 
-  onDataAvailableEvent() {
-    try {
-      this.mediaRecorder.ondataavailable = (event: any) => {
-        if (event.data && event.data.size > 0) {
-          this.recordedBlobs.push(event.data);
-        }
-      };
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // record() {
+  //   if (this.isRecording) {
+  //     this.stopRecording()
+  //   } else {
+  //     this.startRecording()
+  //   }
+  // }
 
-  onStopRecordingEvent() {
-    try {
-      this.mediaRecorder.onstop = (event: Event) => {
-        const videoBuffer = new Blob(this.recordedBlobs, {
-          type: 'video/webm'
-        });
-        this.downloadUrl = window.URL.createObjectURL(videoBuffer); // you can download with <a> tag
-        this.recordVideoElement.src = this.downloadUrl;
-      };
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // startRecording() {
+  //   this.recordedBlobs = [];
+  //   let options: any = { mimeType: 'video/webm'};
+  //
+  //   try {
+  //     this.mediaRecorder = new MediaRecorder(this.stream, options);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  //
+  //   this.mediaRecorder.start(); // collect 100ms of data
+  //   this.isRecording = !this.isRecording;
+  //   this.onDataAvailableEvent();
+  //   this.onStopRecordingEvent();
+  // }
+  //
+  // stopRecording() {
+  //   this.mediaRecorder.stop();
+  //   this.isRecording = !this.isRecording;
+  // }
+
+  // onDataAvailableEvent() {
+  //   try {
+  //     this.mediaRecorder.ondataavailable = (event: any) => {
+  //       if (event.data && event.data.size > 0) {
+  //         this.recordedBlobs.push(event.data);
+  //       }
+  //     };
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  //
+  // onStopRecordingEvent() {
+  //   try {
+  //     this.mediaRecorder.onstop = (event: Event) => {
+  //       const videoBuffer = new Blob(this.recordedBlobs, {
+  //         type: 'video/webm'
+  //       });
+  //       this.downloadUrl = window.URL.createObjectURL(videoBuffer); // you can download with <a> tag
+  //       this.recordVideoElement.src = this.downloadUrl;
+  //     };
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 }
